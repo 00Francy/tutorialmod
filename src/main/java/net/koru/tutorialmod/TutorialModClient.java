baf92461084c019a549eeb57ea3df4e94bf6a0d4
Francesco Ferrentino <francescoferrentino2008@gmail.com>
2025-03-16 07:18:45 +0100

    Initial commit

---
+++ src/main/java/net/koru/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.koru.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @java.lang.Override
+    public void onInitializeClient() {
+
+    }
+}
